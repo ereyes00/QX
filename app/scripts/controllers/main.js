d7a276950e57fb2f41b0358768bc29d4335f7843
greg <gregborrelly@gmail.com>
2017-05-17 12:16:26 -0400

    this is

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name qxApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the qxApp
+ */
+angular.module('qxApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
